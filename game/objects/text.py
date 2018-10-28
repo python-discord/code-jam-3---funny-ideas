865ce126f5a0de3436de851ec8200a64fe61185f
Leon Sandøy <leon.sandoy@conmodo.com>
2018-10-28 23:02:02 +0100

    Abstracted TextObject into its own object, and TextShootObject now inherits and expands on that.

--- game/objects/text.py
+++ game/objects/text.py
@@ -1,53 +1,24 @@
-import keyword
-import random
 from pathlib import Path
 from typing import Tuple
 
 from pygame.font import Font
-from pygame.surface import Surface
 
 from game.objects import BaseObject
 
-FONT_COLOUR = (255, 255, 255)
-FONT_COLOUR_TYPED = (255, 0, 0)
-FONT_PATH = Path("game", "assets", "fonts", "FiraMono-Regular.ttf")
-FONT_SIZE = 32
-FONT = Font(str(FONT_PATH), FONT_SIZE)
-
 
 class TextObject(BaseObject):
-    def __init__(self, location: Tuple[int, int]):
-        self.word = random.choice(keyword.kwlist).upper()
-        self.typed = random.randint(0, len(self.word))
+    def __init__(
+            self,
+            location: Tuple[int, int],
+            word: str,
+            *,
+            font_path: Path = Path("game", "assets", "fonts", "FiraMono-Regular.ttf"),
+            font_size: int = 32,
+            font_color: Tuple[int, int, int] = (255, 255, 255),
+    ):
+        self.word = word
+        self.font = Font(str(font_path), font_size)
 
-        surface = FONT.render(self.word, True, FONT_COLOUR)
+        surface = self.font.render(self.word, True, font_color)
 
         super().__init__(location, surface)
-
-    def draw(self):
-        if not self.typed:
-            self.image = FONT.render(self.word, True, FONT_COLOUR)
-        elif self.typed >= len(self.word):
-            self.image = FONT.render(self.word, True, FONT_COLOUR_TYPED)
-        else:
-            typed_text = self.word[:self.typed]
-            untyped_text = self.word[self.typed:]
-
-            typed_surface: Surface = FONT.render(typed_text, True, FONT_COLOUR_TYPED)
-            untyped_surface: Surface = FONT.render(untyped_text, True, FONT_COLOUR)
-
-            self.image = Surface(
-                (
-                    typed_surface.get_width() + untyped_surface.get_width(),
-                    typed_surface.get_height() + untyped_surface.get_height()
-                )
-            )
-
-            self.image.blits(
-                (
-                    (typed_surface, (0, 0)),
-                    (untyped_surface, (typed_surface.get_width(), 0))
-                )
-            )
-
-        super(TextObject, self).draw()
---
+++ game/objects/text_shoot.py
@@ -0,0 +1,57 @@
+import keyword
+import random
+from pathlib import Path
+from typing import Tuple
+
+from pygame.font import Font
+from pygame.surface import Surface
+
+from game.objects import TextObject
+
+FONT_COLOUR = (255, 255, 255)
+FONT_COLOUR_TYPED = (255, 0, 0)
+FONT_PATH = Path("game", "assets", "fonts", "FiraMono-Regular.ttf")
+FONT_SIZE = 32
+FONT = Font(str(FONT_PATH), FONT_SIZE)
+
+
+class TextShootObject(TextObject):
+    def __init__(self, location: Tuple[int, int]):
+        self.word = random.choice(keyword.kwlist).upper()
+        self.typed = random.randint(0, len(self.word))
+
+        super().__init__(
+            location,
+            self.word,
+            font_path=FONT_PATH,
+            font_size=FONT_SIZE,
+            font_color=FONT_COLOUR
+        )
+
+    def draw(self):
+        if not self.typed:
+            self.surface = FONT.render(self.word, True, FONT_COLOUR)
+        elif self.typed >= len(self.word):
+            self.surface = FONT.render(self.word, True, FONT_COLOUR_TYPED)
+        else:
+            typed_text = self.word[:self.typed]
+            untyped_text = self.word[self.typed:]
+
+            typed_surface: Surface = FONT.render(typed_text, True, FONT_COLOUR_TYPED)
+            untyped_surface: Surface = FONT.render(untyped_text, True, FONT_COLOUR)
+
+            self.surface = Surface(
+                (
+                    typed_surface.get_width() + untyped_surface.get_width(),
+                    typed_surface.get_height() + untyped_surface.get_height()
+                )
+            )
+
+            self.surface.blits(
+                (
+                    (typed_surface, (0, 0)),
+                    (untyped_surface, (typed_surface.get_width(), 0))
+                )
+            )
+
+        super(TextObject, self).draw()
