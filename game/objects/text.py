b6cbd4f0a2321aa1c3d8372b2f2f8982d92c4780
Leon Sandøy <leon.sandoy@conmodo.com>
2018-10-28 23:43:57 +0100

    Fixing the linting error in TextObject

--- game/objects/text.py
+++ game/objects/text.py
@@ -12,12 +12,13 @@ class TextObject(BaseObject):
             location: Tuple[int, int],
             word: str,
             *,
-            font_path: Path = Path("game", "assets", "fonts", "FiraMono-Regular.ttf"),
+            font_path: Path = None,
             font_size: int = 32,
             font_color: Tuple[int, int, int] = (255, 255, 255),
     ):
+        self.font_path = font_path if font_path else Path("game", "assets", "fonts", "FiraMono-Regular.ttf")
         self.word = word
-        self.font = Font(str(font_path), font_size)
+        self.font = Font(str(self.font_path), font_size)
 
         surface = self.font.render(self.word, True, font_color)
 
